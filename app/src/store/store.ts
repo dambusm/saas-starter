72f41f1524b73e73870210b8704a4faadbec5985
Will Schaller <willschaller@gmail.com>
2021-12-25 12:09:53 +0000

    Slight refactoring

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -1,23 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { authApiQueries } from './auth/auth-api-queries';
 import { authManagerQueries } from './auth/auth-manager-queries';
 import counterReducer from './counter/slice';
 import { postsManagerQueries } from './posts/posts-manager-queries';
 
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  [postsManagerQueries.reducerPath]: postsManagerQueries.reducer,
+  [authManagerQueries.reducerPath]: authManagerQueries.reducer,
+  [authApiQueries.reducerPath]: authApiQueries.reducer,
+});
+
 const store = configureStore({
   devTools: true,
-  reducer: {
-    counter: counterReducer,
-    [postsManagerQueries.reducerPath]: postsManagerQueries.reducer,
-    [authManagerQueries.reducerPath]: authManagerQueries.reducer,
-    [authApiQueries.reducerPath]: authApiQueries.reducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(postsManagerQueries.middleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
 
