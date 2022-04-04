0c4179e5b85eeefa2852b2c2f1ad919c27a14204
Will Schaller <willschaller@gmail.com>
2022-04-04 20:17:29 +0200

    Update netlify deployment info

--- scripts/src/setup-frontend-deployment-netlify.ts
+++ scripts/src/setup-frontend-deployment-netlify.ts
@@ -1,3 +1,3 @@
 import { $ } from 'zx';
 
-await $`yarn netlify logout && yarn netlify login`;
+await $`echo "Go to https://app.netlify.com/start, connect to Github and select your repo. Ignore the setup form as it will be overwritten by the config files in the repo. This should be all you need to do."`;
